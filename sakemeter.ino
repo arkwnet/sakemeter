1bceecabdaf1af9e5582efbabe9dcf8fa33db3ec
Sora Arakawa <mail@arkw.net>
2025-08-20 21:16:29 +0900

    Change image split position

--- sakemeter.ino
+++ sakemeter.ino
@@ -1,5 +1,7 @@
 #include <M5Stack.h>
-#include "img/background.h"
+#include "img/header.h"
+#include "img/contents.h"
+#include "img/footer.h"
 
 TFT_eSprite sprite(&M5.Lcd);
 const int VERSION[3] = { 1, 0, 0 };
@@ -11,26 +13,22 @@ void setup() {
   M5.begin();
   M5.Lcd.setSwapBytes(true);
   M5.Lcd.setBrightness(255);
-  sprite.createSprite(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+  sprite.createSprite(SCREEN_WIDTH, 102);
 }
 
 void loop() {
   M5.update();
   if (count % 10 == 0) {
     M5.Lcd.startWrite();
-    // 左上
-    sprite.pushImage(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, background);
+    // Header
+    sprite.pushImage(0, 0, SCREEN_WIDTH, 58, header);
     sprite.pushSprite(0, 0);
-    // 左下
-    sprite.pushImage(0, -1 * (SCREEN_HEIGHT / 2), 320, 240, background);
-    sprite.pushSprite(0, SCREEN_HEIGHT / 2);
-    // 右上
-    sprite.pushImage(-1 * (SCREEN_WIDTH / 2), 0, 320, 240, background);
-    sprite.pushSprite(SCREEN_WIDTH / 2, 0);
-    // 右下
-    sprite.pushImage(-1 * (SCREEN_WIDTH / 2), -1 * (SCREEN_HEIGHT / 2), 320, 240, background);
-    sprite.pushSprite(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
-    M5.Lcd.endWrite();
+    // Main
+    sprite.pushImage(0, 0, SCREEN_WIDTH, 80, contents);
+    sprite.pushSprite(0, 58);
+    // Footer
+    sprite.pushImage(0, 0, SCREEN_WIDTH, 102, footer);
+    sprite.pushSprite(0, 138);
   }
   count++;
   if (count == 30) {
